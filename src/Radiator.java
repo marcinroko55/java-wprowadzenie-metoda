77d3b3853220d56d78cfe351ac90473ae3707ccb
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-08-19 11:46:30 +0200

    #1 realizacja zadania 1, przechowywanie informacji o kaloryferach, metoda void, bezparametrowa

---
+++ src/Radiator.java
@@ -0,0 +1,23 @@
+public class Radiator {
+    char[] color;
+    double weight;
+    int power;
+//po co tutaj to przypisujemy?
+    Radiator(char[] color, double weight, int power){
+        this.color = color;
+        this.weight = weight;
+        this.power = power;
+    }
+// czy w metodzie zawsze nazwy pól?
+    void display(){
+        System.out.println("----------------");
+        System.out.print("Colour: ");
+        for (char c : color) {
+            System.out.print(c);
+
+        }
+        System.out.println();
+        System.out.println("Weight: " + weight);
+        System.out.println("Power: " + power + "W");
+    }
+}
